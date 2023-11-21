3eec3ba558713dca5b046f75ccdf309fe64edff9
NuthalapatiDinesh <112842345+NuthalapatiDinesh@users.noreply.github.com>
2023-11-21 09:59:23 +0530

    Update EXP6.cpp

--- EXP6.cpp
+++ EXP6.cpp
@@ -3,10 +3,10 @@ int main() {
     int n, sum = 0;
     printf("Enter the value of 'n': ");
     scanf("%d", &n);
-	for (int i = 1; i <= n; i++) {
+    for (int i = 2; i <= n; i += 2) {
         sum += i;
     }
-    printf("Sum of the series 1 + 2 + 3 + ... + %d is: %d\n", n, sum);
+    printf("Sum of the series 2 + 4 + 6 + ... + %d is: %d\n", n, sum);
     return 0;
 }
 
